739396ad82198b6b1f3f11c2852a3181a1f5bf8e
Sonia Farbehi <soniafarbehi76@gmail.com>
2023-11-17 14:14:51 -0500

    added some of the logic to details page/components

--- industry-project-client/src/App.jsx
+++ industry-project-client/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import EventListPage from './pages/EventListPage/EventListPage';
+import EventDetailsPage from './pages/EventDetailsPage/EventDetailsPage';
+
 
 const App = () => {
   return (
@@ -8,6 +10,7 @@ const App = () => {
       <Routes>
         {/* Use = instead of {} for the element prop */}
         <Route path='/eventlist' element={<EventListPage />} />
+        <Route path='/eventdetails' element={<EventDetailsPage />} />
       </Routes>
     </BrowserRouter>
   );
