eb94be1f01a4ff107e4d66b1a0ea13248be1a480
Joshua Jeyaranjan <joshuajeyaranjan@gmail.com>
2023-11-17 12:52:34 -0500

    close to getting data for events

--- industry-project-client/src/App.jsx
+++ industry-project-client/src/App.jsx
@@ -1,45 +1,15 @@
-// HI SONIA
 import './App.scss';
-import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EventListPage from './pages/EventListPage/EventListPage';
 
 const App = () => {
-  const [eventNames, setEventNames] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('http://localhost:8081/events');
-      const result = await response.json();
-
-      // Log the entire result for better inspection
-      console.log('Full Result:', result);
-
-      // Ensure the 'events' property exists and is an array
-      const fetchedEvents = Array.isArray(result) ? result : [];
-
-      // Extract the first 50 event names
-      const first50EventNames = fetchedEvents
-        .slice(0, 50) // Take the first 50 events
-        .map(event => event.calEvent.eventName); // Adjusted property path
-
-      setEventNames(first50EventNames);
-    } catch (error) {
-      console.error('Error fetching events:', error);
-    }
-  };
-
   return (
-    <div>
-      <h1>Event Names (First 50)</h1>
-      <ul>
-        {eventNames.map((eventName, index) => (
-          <li key={index}>{eventName}</li>
-        ))}
-      </ul>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Use = instead of {} for the element prop */}
+        <Route path='/eventlist' element={<EventListPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- industry-project-client/src/pages/EventListPage/EventListPage.jsx
+++ industry-project-client/src/pages/EventListPage/EventListPage.jsx
@@ -1,9 +1,8 @@
-// HI SONIA
-import './EventListPage.scss';
 import React, { useState, useEffect } from 'react';
+import EventTile from '../../components/EventTile/EventTile'; // Import the EventTile component
 
 const EventListPage = () => {
-  const [eventNames, setEventNames] = useState([]);
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -12,33 +11,34 @@ const EventListPage = () => {
   const fetchData = async () => {
     try {
       const response = await fetch('http://localhost:8081/events');
+      console.log('Raw Response:', response); // Log the raw response
       const result = await response.json();
-
+      console.log(result);
+  
       // Log the entire result for better inspection
       console.log('Full Result:', result);
-
+  
       // Ensure the 'events' property exists and is an array
       const fetchedEvents = Array.isArray(result) ? result : [];
 
-      // Extract the first 50 event names
-      const first50EventNames = fetchedEvents
-        .slice(0, 50) // Take the first 50 events
-        .map(event => event.calEvent.eventName); // Adjusted property path
+      setEvents(fetchedEvents);
 
-      setEventNames(first50EventNames);
+      
+  
+      // Set the events in state
+      
     } catch (error) {
       console.error('Error fetching events:', error);
     }
   };
-
+  
   return (
     <div>
-      <h1>Event Names (First 50)</h1>
-      <ul>
-        {eventNames.map((eventName, index) => (
-          <li key={index}>{eventName}</li>
-        ))}
-      </ul>
+      <h1>Event List</h1>
+      {events.map((event, index) => (
+  <EventTile key={index} event={event} />
+))}
+
     </div>
   );
 };
