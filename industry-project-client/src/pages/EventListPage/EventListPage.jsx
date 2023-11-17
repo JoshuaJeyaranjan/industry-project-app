ef89340922690517038fbc0e64b8dda57a99f101
Joshua Jeyaranjan <joshuajeyaranjan@gmail.com>
2023-11-17 10:39:17 -0500

    whats up sonia

--- industry-project-client/src/pages/EventListPage/EventListPage.jsx
+++ industry-project-client/src/pages/EventListPage/EventListPage.jsx
@@ -1,7 +1,9 @@
+// HI SONIA
+import './EventListPage.scss';
 import React, { useState, useEffect } from 'react';
 
 const EventListPage = () => {
-  const [events, setEvents] = useState([]);
+  const [eventNames, setEventNames] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -11,7 +13,19 @@ const EventListPage = () => {
     try {
       const response = await fetch('http://localhost:8081/events');
       const result = await response.json();
-      setEvents(result.events);
+
+      // Log the entire result for better inspection
+      console.log('Full Result:', result);
+
+      // Ensure the 'events' property exists and is an array
+      const fetchedEvents = Array.isArray(result) ? result : [];
+
+      // Extract the first 50 event names
+      const first50EventNames = fetchedEvents
+        .slice(0, 50) // Take the first 50 events
+        .map(event => event.calEvent.eventName); // Adjusted property path
+
+      setEventNames(first50EventNames);
     } catch (error) {
       console.error('Error fetching events:', error);
     }
@@ -19,9 +33,11 @@ const EventListPage = () => {
 
   return (
     <div>
-      <h1>Events</h1>
+      <h1>Event Names (First 50)</h1>
       <ul>
- 
+        {eventNames.map((eventName, index) => (
+          <li key={index}>{eventName}</li>
+        ))}
       </ul>
     </div>
   );
