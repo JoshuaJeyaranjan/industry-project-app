0c4597eb82985d0c195e924d0df63de96c6dc511
Sonia Farbehi <soniafarbehi76@gmail.com>
2023-11-17 07:00:38 -0500

    base filing added for componants

---
+++ industry-project-client/src/componants/EventTile/EventTile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EventTile.scss';
+
+const EventTile = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EventTile;
